efac62f13eb32c59a42440169770e9f43a1e4b0b
Igor Dudetskiy <116377166+IgorDudetskiy@users.noreply.github.com>
2023-01-16 18:41:19 +0200

    hw1 method print()

--- src/JavaGit/lesson1/Homework1.java
+++ src/JavaGit/lesson1/Homework1.java
@@ -3,6 +3,7 @@ package JavaGit.lesson1;
 public class Homework1 {
     public static void main(String[] args) {
         task1();
+        System.out.println("first attempt ");
     }
     private static void task1(){
         System.out.println("Homework!");
---
+++ src/JavaGit/lesson1new/hw1.java
@@ -0,0 +1,11 @@
+package JavaGit.lesson1new;
+
+public class hw1 {
+    public static void main(String[] args) {
+        System.out.println("main");
+        print();
+    }
+    public static void print(){
+        System.out.println("print method");
+    }
+}
